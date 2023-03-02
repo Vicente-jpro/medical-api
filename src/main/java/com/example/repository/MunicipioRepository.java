907ec779bf1842ffc87f0a28831ec21805af0188
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-02 21:11:20 +0100

    MunicipioException added

--- src/main/java/com/example/repository/MunicipioRepository.java
+++ src/main/java/com/example/repository/MunicipioRepository.java
@@ -1,11 +1,16 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.models.Municipio;
+import com.example.models.Provincia;
 
 @Repository
 public interface MunicipioRepository extends JpaRepository<Municipio, Integer>{
+	
+	List<Municipio> findByProvincia(Provincia provincia);
 
 }
--- src/main/java/com/example/services/MunicipioService.java
+++ src/main/java/com/example/services/MunicipioService.java
@@ -1,5 +1,10 @@
 package com.example.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MunicipioService {
 
+	
+	
 }
