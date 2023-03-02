23e91060a09443a0a4052da3bddb73ba810cb3c1
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-02 18:40:51 +0100

    ProvinciaService added

---
+++ src/main/java/com/example/services/ProvinciaService.java
@@ -0,0 +1,26 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.exceptions.ProvinciaNotFoundException;
+import com.example.models.Provincia;
+import com.example.repository.ProvinciaRepository;
+
+@Service
+public class ProvinciaService {
+
+	private ProvinciaRepository provinciaRepository;
+	
+	public List<Provincia> findAll() {
+		return this.provinciaRepository.findAll();
+	}
+	
+	public Provincia findById(Integer idProvincia) {
+		return this.provinciaRepository
+				.findById(idProvincia)
+				.orElseThrow( () -> new ProvinciaNotFoundException("Código da provincia invalido. id: "+idProvincia));
+	}
+	
+}
